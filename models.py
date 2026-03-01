13576ad18340261e02b3cf27712b57ff18364165
Caktus-boop <denis.chernikov.2009@gmail.com>
2026-03-01 18:18:27 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,25 +1,11 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import ForeignKey
-
 
 class Base(DeclarativeBase):
     pass
 
-
 class Users(Base):
     __tablename__ = 'users'
-
     id: Mapped[int] = mapped_column(primary_key=True)
     tg_id: Mapped[str]
     name: Mapped[str]
-    place_id: Mapped[int | None] = mapped_column(
-        ForeignKey('timetable.id', ondelete='CASCADE')
-    )
-
-
-class Timetable(Base):
-    __tablename__ = 'timetable'
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    floor: Mapped[int]
-    pos: Mapped[int]
+    place_id: Mapped[int | None]
