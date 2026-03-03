83dc280ca4262105204c089980f02564bdfc49cb
Caktus-boop <denis.chernikov.2009@gmail.com>
2026-03-03 21:58:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -82,10 +82,10 @@ async def scheduler():
     while True:
         now = datetime.now()
 
-        if now.weekday() == 1 and now.hour == 0 and now.minute == 0:
+        if now.weekday() == 1 and now.hour == 22 and now.minute == 2:
             await random_place()
 
-        if now.weekday() == 5 and now.hour == 0 and now.minute == 0:
+        if now.weekday() == 1 and now.hour == 22 and now.minute == 0:
             await clear_timetable()
 
         await asyncio.sleep(60)
