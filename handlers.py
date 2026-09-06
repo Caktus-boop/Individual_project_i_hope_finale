c051b900fab1f8f825ef78ecd28399234b01b8ad
Caktus-boop <denis.chernikov.2009@gmail.com>
2026-09-06 21:16:32 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -12,7 +12,7 @@ from sqlalchemy import select, update
 
 router = Router()
 
-ADMIN_IDS = {"1377739047", "263585469","1398362563"}  # Черников Денис, Захарова Олеся, Маришка
+ADMIN_IDS = {"1377739047", "1675113168","1398362563"}  # Черников Денис, Захарова Олеся, Маришка
 DENIS_ID = "1377739047"
 
 class AdminStates(StatesGroup):
